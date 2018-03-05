8f313968cdc7e4818d1cf2d6593c3b4b5252b1f7
Jae Anne Dientes <dientesjam97@gmail.com>
2018-03-05 23:08:21 +0800

    third commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';  
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCO_5_DeDPwE43mlw0Q4EefGWN7qS8ULs8",
+    authDomain: "contactlist-574be.firebaseapp.com",
+    databaseURL: "https://contactlist-574be.firebaseio.com",
+    projectId: "contactlist-574be",
+    storageBucket: "contactlist-574be.appspot.com",
+    messagingSenderId: "14137441684"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  
+    AngularFirestoreModule,    
+    FormsModule                        
+  ],
+  providers: [], 
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
